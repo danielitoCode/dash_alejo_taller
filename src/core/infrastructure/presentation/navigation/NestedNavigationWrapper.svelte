9648e1c1bee3232a3c92332ec24386bb7cd1ecc8
Daniel Imbert <daniel.imbert96@gmail.com>
2026-08-18 12:51:30 -0400

    temp: remove broken nav file before restore

--- src/core/infrastructure/presentation/navigation/NestedNavigationWrapper.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    // See full implementation in follow-up - temporary stub to avoid broken build
-    export let navController: any;
-    export let navBackStackEntry: any;
-</script>
-<p>Cargando navegación…</p>
