e5b97a7d4d70df00852187b5e1d3a35fcdcfcd07
Daniel Imbert <daniel.imbert96@gmail.com>
2026-08-28 11:16:01 -0400

    fix(purchase.store): return RegisterPurchaseEntryResult for priceProtections typing

--- src/core/feature/purchase/presentation/viewmodel/purchase.store.ts
+++ src/core/feature/purchase/presentation/viewmodel/purchase.store.ts
@@ -1,9 +1,14 @@
 import { purchaseContainer } from "../../di/purchase.container"
-import type { RegisterPurchaseEntryInput } from "../../domain/caseuse/RegisterPurchaseEntryCaseUse"
+import type {
+    RegisterPurchaseEntryInput,
+    RegisterPurchaseEntryResult,
+} from "../../domain/caseuse/RegisterPurchaseEntryCaseUse"
 import type { PurchaseEntry, PurchaseEntryLine } from "../../domain/entity/PurchaseEntry"
 import type { Supplier } from "../../domain/entity/Supplier"
 
-async function registerPurchaseEntry(input: RegisterPurchaseEntryInput): Promise<PurchaseEntry> {
+async function registerPurchaseEntry(
+    input: RegisterPurchaseEntryInput
+): Promise<RegisterPurchaseEntryResult> {
     return purchaseContainer.useCases.registerPurchaseEntry.execute(input)
 }
 
